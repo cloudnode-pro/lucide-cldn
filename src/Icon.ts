f6f8c9db11f353d2752b71dd881f257ffa310ee2
Zefir Kirilov <contact@zefir.pro>
2025-04-27 23:17:50 +0300

    make Icon class abstract

--- src/Icon.ts
+++ src/Icon.ts
@@ -19,7 +19,7 @@ import {SvgComponent} from "@cldn/components";
 /**
  * A Lucide icon component.
  */
-export class Icon extends SvgComponent {
+export abstract class Icon extends SvgComponent {
     protected constructor(element: SVGSVGElement) {
         super(element);
         this.set("ariaHidden", "true");
