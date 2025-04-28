383ca3f444d3878fb3f4904ad11c3bf49163fbc5
Zefir Kirilov <contact@zefir.pro>
2025-04-28 17:32:34 +0300

    simplify SVGO config and increase optimisation

--- gen/optimiseSvg.ts
+++ gen/optimiseSvg.ts
@@ -16,159 +16,30 @@
  */
 import {optimize as svgo} from "svgo";
 
-let prefixCounter = 0;
-
 export async function optimiseSvg(svg: string) {
     return svgo(svg, {
         multipass: true,
+        js2svg: {pretty: false},
         plugins: [
             {
-                name: "cleanupAttrs",
+                name: "preset-default",
                 params: {
-                    newlines: true,
-                    trim: true,
-                    spaces: true,
-                },
-            },
-            {
-                name: "cleanupIds",
-                params: {
-                    remove: true,
-                    minify: true,
-                    preserve: [],
-                    preservePrefixes: [],
-                    force: false,
-                },
-            },
-            {
-                name: "cleanupListOfValues",
-                params: {
-                    floatPrecision: 1,
-                    leadingZero: true,
-                    defaultPx: true,
-                    convertToPx: false,
-                },
-            },
-            {
-                name: "cleanupNumericValues",
-                params: {
-                    floatPrecision: 1,
-                    leadingZero: false,
-                    defaultPx: true,
-                    convertToPx: false,
-                },
-            },
-            "collapseGroups",
-            "convertColors",
-            "convertEllipseToCircle",
-            "convertOneStopGradients",
-            {
-                name: "convertPathData",
-                params: {
-                    applyTransforms: true,
-                    applyTransformsStroked: true,
-                    makeArcs: {
-                        threshold: 2.5,
-                        tolerance: 0.5,
+                    overrides: {
+                        removeViewBox: false,
                     },
-                    straightCurves: true,
-                    convertToQ: true,
-                    lineShorthands: true,
-                    convertToZ: true,
-                    curveSmoothShorthands: true,
-                    floatPrecision: 1,
-                    transformPrecision: 0,
-                    smartArcRounding: true,
-                    removeUseless: true,
-                    collapseRepeated: true,
-                    utilizeAbsolute: true,
-                    forceAbsolutePath: true,
                 },
             },
-            {
-                name: "convertStyleToAttrs",
-                params: {
-                    keepImportant: true,
-                },
-            },
-            {
-                name: "convertTransform",
-                params: {
-                    convertToShorts: true,
-                    degPrecision: 1,
-                    floatPrecision: 1,
-                    transformPrecision: 0,
-                    matrixToTransform: true,
-                    shortTranslate: true,
-                    shortScale: true,
-                    shortRotate: true,
-                    removeUseless: true,
-                    collapseIntoOne: true,
-                },
-            },
-            {
-                name: "inlineStyles",
-                params: {
-                    onlyMatchedOnce: true,
-                    removeMatchedSelectors: true,
-                    useMqs: [""],
-                    usePseudos: [""],
-                },
-            },
-            {
-                name: "mergePaths",
-                params: {
-                    floatPrecision: 1,
-                    noSpaceAfterFlags: true,
-                },
-            },
-            "mergeStyles",
-            {
-                name: "minifyStyles",
-                params: {
-                    usage: {
-                        tags: true,
-                        ids: true,
-                        classes: true,
-                        force: false,
-                    }
-                },
-            },
-            "moveElemsAttrsToGroup",
-            {
-                name: "prefixIds",
-                params: {
-                    delim: '',
-                    prefix: () => String(++prefixCounter),
-                    prefixIds: true,
-                    prefixClassNames: false
-                },
-            },
-            "removeComments",
-            "removeDesc",
-            "removeDimensions",
-            "removeDoctype",
-            "removeEditorsNSData",
-            "removeEmptyAttrs",
-            "removeEmptyContainers",
-            "removeEmptyText",
-            "removeHiddenElems",
-            "removeMetadata",
-            "removeOffCanvasPaths",
-            "removeRasterImages",
-            "removeScriptElement",
-            "removeStyleElement",
-            "removeTitle",
-            "removeUnknownsAndDefaults",
-            "removeUnusedNS",
-            "removeUselessDefs",
-            "removeUselessStrokeAndFill",
             "removeXMLNS",
-            "removeXMLProcInst",
-            "removeXlink",
-            "reusePaths",
-            "sortAttrs",
-            "sortDefsChildren",
+            "removeScriptElement",
+            "convertStyleToAttrs",
+            "removeStyleElement",
+            {
+                name: "removeAttrs",
+                params: {
+                    attrs: ["class"],
+                },
+            },
+            "removeDimensions",
         ],
     });
 }
